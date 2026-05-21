b43b1f7e2c0e90cbf36000278bc539af284512bb
dima55279 <dima55279@yandex.ru>
2026-05-22 01:42:53 +0300

    Fixed embeddings.py

--- app/core/embeddings.py
+++ app/core/embeddings.py
@@ -1,31 +1,77 @@
 from sentence_transformers import SentenceTransformer
+import numpy as np
 
 from app.core.config import EMBEDDING_MODEL
+from app.utils.logger import get_logger
 
-_model = None
+
+logger = get_logger(__name__)
+
+_model = "BAAI/bge-m3"
+
+
+def normalize(vectors):
+
+    vectors = np.array(vectors)
+
+    norms = np.linalg.norm(
+        vectors,
+        axis=1,
+        keepdims=True
+    )
+
+    return vectors / norms
 
 
 def get_embedding_model():
+
     global _model
 
     if _model is None:
-        _model = SentenceTransformer(EMBEDDING_MODEL)
+
+        logger.info(
+            f"Loading embeddings model: "
+            f"{EMBEDDING_MODEL}"
+        )
+
+        _model = SentenceTransformer(
+            EMBEDDING_MODEL
+        )
+
+        logger.info("Embedding model loaded")
 
     return _model
 
 
 def embed_texts(texts):
+
     model = get_embedding_model()
-    return model.encode(
+
+    texts = [
+        f"passage: {t}"
+        for t in texts
+    ]
+
+    embeddings = model.encode(
         texts,
-        normalize_embeddings=True,
-        show_progress_bar=False
+        show_progress_bar=True
     )
+
+    return normalize(embeddings)
 
 
 def embed_query(query):
+
     model = get_embedding_model()
-    return model.encode(
-        [query],
-        normalize_embeddings=True
-    )[0]
+
+    query = f"query: {query}"
+
+    embedding = model.encode(query)
+
+    embedding = np.array(embedding)
+
+    embedding = embedding / np.linalg.norm(
+        embedding
+    )
+
+    return embedding
